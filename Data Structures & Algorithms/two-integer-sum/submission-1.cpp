ee872c241e30f2b2d524d9053d07854122dc78cb
TANUJ PARASHAR <tanuj.parashar.ug23@nsut.ac.in>
2026-08-20 22:05:45 +0530

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        unordered_map<int,int> mp;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int diff=target-nums[i];
+            if(mp.find(diff)!=mp.end()){
+                ans.push_back(mp[diff]);
+                ans.push_back(i);
+                return ans;
+            }
+            mp[nums[i]]=i;
+        }
+        return ans;
+        
+    }
+};
