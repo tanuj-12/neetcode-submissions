b74e007a5130d35b39b8b4def7c8620b37e47fd6
TANUJ PARASHAR <tanuj.parashar.ug23@nsut.ac.in>
2026-09-13 23:30:54 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int lmax=0;
+        int rmax=0;
+        int total=0;
+        int l=0;
+        int r=height.size()-1;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(lmax>height[l]){
+                    total+=lmax-height[l];
+                }
+                else{
+                    lmax=height[l];
+                }
+                l++;
+            }
+            else{
+                if(rmax>height[r]){
+                    total+=rmax-height[r];
+                }
+                else{
+                    rmax=height[r];
+                }
+                r--;
+            }
+        }
+        return total;
+
+
+        
+    }
+};
